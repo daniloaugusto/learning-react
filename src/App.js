1742d717f8934c28aac78f54ba2fe0d2ceddefb8
Danilo Augusto <daniloaugustogrs@gmail.com>
2019-10-27 15:00:42 -0300

    Getting the recipes from api
    Creating a function that get the recipes from the
    API and put in a state os recipes.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React, {useEffect}  from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
   const { REACT_APP_APP_ID: APP_ID, REACT_APP_APP_KEY: APP_KEY } = process.env;
 
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const api_default_url = 'https://api.edamam.com/';
+    const api_query_url = `search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+    const response = await fetch(api_default_url + api_query_url);
+    const data = await response.json();
+    
+    setRecipes(data.hits);
+  }
 
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
     </div>
   );
 }
